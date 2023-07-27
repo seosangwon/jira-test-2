313a9a026f1e99ac6337f72fd36743dcad232c08
seosang-won <zmdk1205@naver.com>
2023-07-27 11:20:20 +0900

    initial commits2

---
+++ src/main/java/com/example/jiratest2/JiraTest2Application.java
@@ -0,0 +1,14 @@
+package com.example.jiratest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiraTest2Application.class, args);
+        System.out.println("지라테스트 2번째 입니당");
+    }
+
+}
