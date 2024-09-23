3aeff976d8f818e2f6e44ad6b2caa258653b498f
seosang-won <zmdk1205@naver.com>
2024-09-23 14:16:05 +0900

    feat : TT-4 last test

--- src/main/java/com/example/jiratest2/Member.java
+++ src/main/java/com/example/jiratest2/Member.java
@@ -1,8 +1,10 @@
 package com.example.jiratest2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @Controller
+@RestController
 public class Member {
 }
