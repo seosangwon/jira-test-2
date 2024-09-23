e707cddb1ee7c41b08ce5abf2f1a5e87f4220d65
seosang-won <zmdk1205@naver.com>
2024-09-23 11:42:19 +0900

    init

---
+++ src/main/java/com/example/jiratest2/JiraTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.jiratest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiraTest2Application.class, args);
+    }
+
+}
